721d5d2a721b64cd4f876f399a46d7cd639217f6
lvl01rat <ecorreia@email.neit.edu>
2025-11-20 17:55:15 -0500

    starting over at the flappy bird clone because I hate the state machine

---
+++ challenges.js
@@ -0,0 +1,86 @@
+import { PipeObstacle } from "./pipeObstacle.js";
+import { Bird } from "./bird.js";
+
+let canvas = document.getElementById("myCanvas");
+let pencil = canvas.getContext("2d"); // This gives you the drawing context, like a pencil
+
+
+//function for resetting the game
+function resetGame() {
+    console.log("resetting game")
+
+    bird.x = 50;
+    bird.y = 50;
+
+    testPipe = new PipeObstacle(canvas, pencil);
+}
+
+function gameLoop() {
+    
+    // Draw background
+    pencil.clearRect(0, 0, canvas.width, canvas.height);
+    pencil.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+
+   
+
+   
+    testPipe.move();
+    testPipe.draw();
+
+    bird.gravity();
+    bird.draw();
+
+    let wasHit = bird.isHitByPipe(testPipe);
+    if(wasHit) {
+        console.log("you're dead, comrade!");
+        score = -1
+        resetGame();
+    }
+
+     let getHit = bird.getsHitByPipe(testPipe);
+    if(getHit) {
+        console.log("you're dead, comrade!");
+        score = -1
+        resetGame();
+
+       
+    } 
+    
+    if (bird.y <= 0 || bird.y + bird.height >= canvas.height) {
+            console.log("No cheating, comrade!");
+            score = -1
+            resetGame();
+        }
+    
+}
+
+
+setInterval(gameLoop, 50);
+
+let score = 0;
+
+//score goes up every second
+function raiseScore() {
+    score += 1;
+    let scoreElement = document.getElementById("scoreDisplay");
+    scoreElement.innerHTML = "SCORE:" + score;
+}
+setInterval(raiseScore, 1000);
+
+function detectClick() {
+    bird.flap();
+}
+
+function detectKey() {
+    bird.flap();
+
+}
+
+canvas.addEventListener("click", detectClick);
+document.addEventListener("keypress", detectKey)
+
+let testPipe = new PipeObstacle(canvas, pencil);
+testPipe.draw();
+
+let bird = new Bird(canvas, pencil);
